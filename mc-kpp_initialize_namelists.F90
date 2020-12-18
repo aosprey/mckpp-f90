8ce7b1e38d6e0510140ddbc0da3204b196fb684b
Annette Osprey <4313200+aosprey@users.noreply.github.com>
2020-12-18 14:52:53 +0000

    Split out initialize fields and namelist routines into separate files.

---
+++ mc-kpp_initialize_namelists.F90
@@ -0,0 +1,312 @@
+#ifdef MCKPP_CAM3
+#include <misc.h>
+#include <params.h>
+SUBROUTINE MCKPP_INITIALIZE_NAMELIST
+  USE mckpp_types, only: kpp_const_fields
+#else
+SUBROUTINE MCKPP_INITIALIZE_NAMELIST(kpp_const_fields)
+  USE mckpp_data_fields
+#endif /*MCKPP_CAM3*/
+  USE mckpp_parameters
+
+  IMPLICIT NONE
+
+#include <landsea.com>
+#include <constants.com>
+#include <times.com>
+#include <timocn.com>
+#include <location.com>
+#include <vert_pgrid.com>
+#include <proc_swit.com>
+#include <proc_pars.com>
+#include <initialcon.com>
+#include <ocn_advec.com>
+#include <ocn_state.com>
+#include <ocn_paras.com>
+#include <ice_paras.com>
+#include <flx_paras.com>
+#include <flx_in.com>
+#include <output.com>
+#include <couple.com>
+#include <sstclim.com>
+#include <fcorr_in.com>
+#include <sfcorr_in.com>
+#include <relax_3d.com>
+#include <bottomclim.com>
+#include <currclim.com>
+
+#ifndef MCKPP_CAM3
+  TYPE(kpp_const_type) :: kpp_const_fields
+#endif
+  
+  ! Local variables    
+  REAL :: alat,alon,delta_lat,delta_lon,dscale
+  INTEGER :: i,j,k,l,ipt,ix,iy
+
+  NAMELIST/NAME_PARAMETERS/nz, ndim, nx, ny, nvel, nsclr, nsb, itermax, hmixtolfrac, & 
+       ngrid, nzl, nzu, nzdivmax, nztmax, igridmax, & 
+       nsflxs, njdt, ndharm, maxmodeadv, mr, nx_globe, ny_globe
+  NAMELIST/NAME_CONSTANTS/grav,vonk,sbc,twopi,onepi,TK0,spd,dpy,&
+       epsw,albocn,EL,SL,FL,FLSN
+  NAMELIST/NAME_PROCSWIT/LKPP,LRI,LDD,LICE,&
+       LBIO,LNBFLX,LTGRID,LRHS,L_SSref
+  NAMELIST/NAME_DOMAIN/DMAX,alon,alat,delta_lat,delta_lon,&
+       L_STRETCHGRID,dscale,L_REGGRID,L_VGRID_FILE,vgrid_file
+  NAMELIST/NAME_START/ L_INITDATA,initdata_file,L_INTERPINIT,&
+       L_RESTART,restart_infile
+  NAMELIST/NAME_TIMES/ dtsec,startt,finalt,ndtocn,nyear
+  NAMELIST/NAME_ADVEC/ L_ADVECT,advect_file,L_RELAX_SST,&
+       relax_sst_in,relax_sal_in,L_RELAX_CALCONLY,L_RELAX_SAL,&
+       L_RELAX_OCNT,relax_ocnt_in
+  NAMELIST/NAME_PARAS/ paras_file,L_JERLOV
+  NAMELIST/NAME_OUTPUT/ L_RESTARTW,restart_outfile,ndt_per_restart
+  NAMELIST/NAME_FORCING/ L_FLUXDATA,forcing_file,L_FCORR_WITHZ,&
+       fcorrin_file,ndtupdfcorr,L_VARY_BOTTOM_TEMP,ndtupdbottom,&
+       bottomin_file,L_FCORR,L_UPD_FCORR,L_UPD_BOTTOM_TEMP,L_REST,&
+       L_PERIODIC_FCORR,L_PERIODIC_BOTTOM_TEMP,fcorr_period,L_SFCORR_WITHZ,&
+       sfcorrin_file,ndtupdsfcorr,L_SFCORR,L_UPD_SFCORR,L_PERIODIC_SFCORR,&
+       sfcorr_period,bottom_temp_period,sal_file,L_UPD_SAL,L_PERIODIC_SAL,&
+       sal_period,ndtupdsal,ocnt_file,L_UPD_OCNT,L_PERIODIC_OCNT,ocnt_period,&
+       ndtupdocnt,L_NO_FREEZE,L_NO_ISOTHERM,isotherm_bottom,isotherm_threshold,&
+       L_DAMP_CURR,dtuvdamp,L_INTERP_OCNT,ndt_interp_ocnt,L_INTERP_SAL,ndt_interp_sal
+  NAMELIST/NAME_COUPLE/ L_COUPLE,ifirst,ilast,jfirst,jlast,L_CLIMSST,sstin_file,&
+       L_UPD_CLIMSST,ndtupdsst,L_CPLWGHT,cplwght_file,icein_file,L_CLIMICE,L_UPD_CLIMICE,&
+       ndtupdice,L_CLIM_ICE_DEPTH,L_CLIM_SNOW_ON_ICE,L_OUTKELVIN,L_COUPLE_CURRENTS,&
+       currin_file,L_CLIMCURR,L_UPD_CLIMCURR,ndtupdcurr,L_PERIODIC_CLIMICE,L_PERIODIC_CLIMSST,&
+       climsst_period,climice_period
+  NAMELIST/NAME_LANDSEA/ L_LANDSEA,landsea_file
+  
+  ! This is a bug fix for the IBM XLF compiler, which otherwise complains
+  ! about "incorrect characters" in the namelist.  If you are using the
+  ! IBM compiler, you need to pass -WF,-DXLF_OLDNAME when you compile MC-KPP.
+  ! NPK 5/6/09 - R2
+#ifdef XLF_OLDNAME
+  CALL SETRTEOPTS("namelist=old")
+#endif
+  
+  ! Open the namelist
+  OPEN(75,FILE='3D_ocn.nml')  
+
+  ! Read parameters namelist first
+  READ(75,NAME_CONSTANTS) 
+  WRITE(nuout,*) 'KPP : Read Namelist PARAMETERS'
+  nzm1 = nz -1 
+  nzp1 = nz+1
+  npts = nx * ny 
+  nvp1 = nvel + 1 
+  nsp1 = nsclr + 1 
+  nzp1tmax = nztmax + 1 
+  nsflxsm1 = nsflxs - 1
+  nsflxsp2 = nsflxs + 2
+  mrp1 = mr + 1 
+  npts_globe = nx_globe + ny_globe 
+
+  WRITE(nuout,*) "nzm1, nzp1, npts, nvp1, nsp1, nzp1tmax, nsflxsm1, nsflxsp1, mrp1, npts_globe = ",  nzm1, nzp1, npts, nvp1, nsp1, nzp1tmax, nsflxsm1, nsflxsp1, mrp1, npts_globe
+
+#ifndef MCKPP_CAM3
+  CALL mckpp_allocate_const_fields(kpp_const_fields) 
+#endif 
+  allocate(kpp_const_fields%wmt(0:891,0:49))
+  allocate(kpp_const_fields%wst(0:891,0:49))
+  allocate(kpp_const_fields%tri(0:NZtmax,0:1,NGRID))
+
+  ! Initialse and read the constants name list
+  spd=86400.                ! secs/day
+  dpy=360.                  ! days/year
+  twopi=8*atan(1.)          ! 2pi
+  onepi=twopi/2.            ! pi
+  grav=9.816                ! gravity
+  vonk=0.4                  ! Von Karman's constant
+  TK0=273.15                ! Kelvin of 0degC
+  sbc=5.67e-8               ! Stefan Boltzmann Constant
+  epsw=1.0                  ! cor.fac for departure of H2O from B.body
+  albocn=0.06               ! albedo for seawater
+  sice=4.0                  ! salinity of ice(?)
+  EL=2.50e6                 ! Latent heat of evap. at 0C (or constant)
+  SL=2512200.               ! Latent heat of evap for ice
+  FL=334000.                ! Latent heat of fusion for ice
+  FLSN=FL                   ! Latent heat of fusion for snow
+  READ(75,NAME_CONSTANTS)
+  WRITE(nuout,*) 'KPP : Read Namelist CONSTANTS'
+  
+  ! Initialize and read the processes namelist
+  LKPP=.TRUE.
+  LRI=.TRUE.
+  LDD=.FALSE.
+  LICE=.FALSE.
+  LBIO=.FALSE.
+  LTGRID=.FALSE.
+  LNBFLX=.FALSE.
+  LRHS=.FALSE.
+  L_SSref=.TRUE.
+  READ(75,NAME_PROCSWIT)
+  WRITE(nuout,*) 'KPP : Read Namelist PROCSWIT'
+  
+  ! Initilalize and read the location name list
+  DMAX=0.0
+  alat=0.0
+  alon=0.0
+  delta_lat=2.5
+  delta_lon=3.75
+  dscale=0.0
+  L_STRETCHGRID=.FALSE.
+  L_REGGRID=.TRUE.
+  L_VGRID_FILE=.FALSE.
+  READ(75,NAME_DOMAIN)
+  IF (DMAX .LE. 0.0) THEN 
+     WRITE(nuerr,*) 'KPP : You must specify a depth for the domain'
+     CALL MCKPP_ABORT
+  ENDIF
+  IF ((L_STRETCHGRID) .AND. (dscale .EQ. 0.0)) THEN
+     WRITE(nuerr,*) 'KPP : You cannot have dscale=0 for stretched ',&
+          'grids'
+     CALL MCKPP_ABORT
+  ENDIF
+  write(nuout,*) 'KPP : Read Namelist DOMAIN'
+  kpp_const_fields%alat=alat
+  kpp_const_fields%alon=alon
+  kpp_const_fields%delta_lat=delta_lat
+  kpp_const_fields%delta_lon=delta_lon
+  kpp_const_fields%dscale=dscale
+  
+  ! Initialize and read the landsea name list
+  L_LANDSEA=.FALSE.
+  READ(75,NAME_LANDSEA)
+  WRITE(nuout,*) 'KPP : Read Namelist LANDSEA'
+  
+  ! Initialize and read the start name list
+  L_INITDATA= .TRUE.
+  L_INTERPINIT= .TRUE.
+  L_RESTART= .FALSE.
+  WRITE(restart_infile,*) 'fort.30'
+  READ(75,NAME_START) 
+  write(nuout,*) 'KPP : Read Namelist START'
+  
+  ! Initialize and read the times namelist
+  ndtocn=1
+  dtsec=0.0
+  startt=-999.999
+  finalt=-999.999
+  READ(75,NAME_TIMES) 
+  IF ((dtsec .LE. 0.0) .OR. (startt .LT. 0.0) .OR. (finalt .LT. 0.0)) THEN 
+     WRITE(nuerr,*) 'KPP : You must specify values of dtsec,startt,finalt in the namelist'
+     CALL MCKPP_ABORT
+  ENDIF
+  kpp_const_fields%ndtocn=ndtocn
+  kpp_const_fields%spd=spd
+  kpp_const_fields%dtsec=dtsec
+  kpp_const_fields%startt=startt*kpp_const_fields%spd
+  kpp_const_fields%finalt=finalt*kpp_const_fields%spd
+  kpp_const_fields%dto=kpp_const_fields%dtsec/float(kpp_const_fields%ndtocn)
+  kpp_const_fields%nend=int((kpp_const_fields%finalt-kpp_const_fields%startt)/kpp_const_fields%dtsec)
+  kpp_const_fields%nstart=nint(kpp_const_fields%startt)/kpp_const_fields%dto
+  IF (float(kpp_const_fields%nend*kpp_const_fields%ndtocn) .NE. &
+       (kpp_const_fields%finalt-kpp_const_fields%startt)/kpp_const_fields%dto) THEN
+     WRITE(nuerr,*) 'KPP : The integration length is not a multiple of the ocean timestep' 
+     WRITE(nuerr,*) 'dto=',kpp_const_fields%dto
+     WRITE(nuerr,*) 'finalt=',kpp_const_fields%finalt
+     WRITE(nuerr,*) 'startt=',kpp_const_fields%startt
+     CALL MCKPP_ABORT
+  ENDIF
+  kpp_const_fields%startt=kpp_const_fields%startt/kpp_const_fields%spd
+  kpp_const_fields%finalt=kpp_const_fields%finalt/kpp_const_fields%spd
+  kpp_const_fields%time=kpp_const_fields%startt
+  WRITE(nuout,*) 'KPP : Read Namelist TIMES'
+  
+  ! Initialize and read the couple namelist
+#ifdef MCKPP_COUPLE
+  L_COUPLE=.TRUE.
+#else
+  L_COUPLE=.FALSE.
+#endif
+  L_COUPLE_CURRENTS=.FALSE.
+  L_OUTKELVIN=.FALSE.
+  L_UPD_CLIMSST=.FALSE.
+  L_UPD_CLIMICE=.FALSE.
+  L_CLIMICE=.FALSE.
+  L_CLIMSST=.FALSE.
+  L_CLIMCURR=.FALSE. 
+  L_BAD_ICE_DEPTH=.FALSE.
+  ifirst=1
+  ilast=nx
+  jfirst=1
+  jfirst=ny
+  READ(75,NAME_COUPLE)
+  write(nuout,*) 'KPP : Read Namelist COUPLE'
+  
+  ! Initialize and read the advection namelist
+  L_ADVECT=.FALSE.
+  L_RELAX_SST=.FALSE.
+  L_RELAX_CALCONLY=.FALSE.
+  DO iy=1,ny
+     relax_sst_in(iy)=0.0
+     relax_sal_in(iy)=0.0
+  ENDDO
+  READ(75,NAME_ADVEC)
+  
+  ! Initialize and read the paras namelist
+  paras_file='3D_ocnparas.nc'
+  L_JERLOV=.TRUE.
+  READ(75,NAME_PARAS)
+
+  ! Initialize and read the forcing namelist
+  L_FLUXDATA=.FALSE.
+  L_FCORR_WITHZ=.FALSE.
+  L_FCORR=.FALSE.
+  L_UPD_FCORR=.FALSE.
+  L_SFCORR_WITHZ=.FALSE.
+  L_SFCORR=.FALSE.
+  L_UPD_SFCORR=.FALSE.
+  L_UPD_SAL=.FALSE.
+  L_VARY_BOTTOM_TEMP=.FALSE.
+  L_UPD_BOTTOM_TEMP=.FALSE.
+  L_REST=.FALSE.
+  L_NO_FREEZE=.FALSE.
+  L_NO_ISOTHERM=.FALSE.
+  L_DAMP_CURR=.FALSE.
+  forcing_file='1D_ocean_forcing.nc'
+  ocnT_file='none'
+  READ(75,NAME_FORCING)
+  write(nuout,*) 'KPP : Read Namelist FORCING'    
+  IF (L_FCORR_WITHZ .AND. L_FCORR) THEN
+     WRITE(nuerr,*) 'KPP : L_FCORR and L_FCORR_WITHZ are '&
+          //'mutually exclusive.  Choose one or neither.'
+     CALL MCKPP_ABORT
+  ENDIF
+  IF (L_SFCORR_WITHZ .AND. L_SFCORR) THEN
+     WRITE(nuerr,*) 'KPP : L_SFCORR and L_SFCORR_WITHZ are '&
+          //'mutually exclusive.  Choose one or neither.'
+     CALL MCKPP_ABORT
+  ENDIF
+  IF (L_FCORR_WITHZ .AND. L_RELAX_SST) THEN
+     WRITE(nuerr,*) 'KPP : L_FCORR_WITHZ and L_RELAX_SST are '&
+          //'mutually exclusive.  Choose one or neither.'
+     CALL MCKPP_ABORT
+  ENDIF
+  IF (L_NO_ISOTHERM .AND. (ocnT_file .eq. 'none' .or.&
+       sal_file .eq. 'none')) THEN
+     WRITE(nuerr,*) 'KPP : If you specify L_NO_ISOTHERM for '&
+          //'reseting of isothermal points, you must specify files '&
+          //'from which to read climatological ocean temperature '&
+          //'(ocnT_file) and salinity (sal_file).'
+     CALL MCKPP_ABORT
+  ELSEIF (L_NO_ISOTHERM) THEN
+     kpp_const_fields%iso_bot=isotherm_bottom
+     kpp_const_fields%iso_thresh=isotherm_threshold
+  ENDIF
+  IF (L_DAMP_CURR) THEN
+     kpp_const_fields%dt_uvdamp=dtuvdamp
+  ENDIF
+  
+  L_RESTARTW=.TRUE.      
+  kpp_const_fields%ndt_per_restart=kpp_const_fields%nend*kpp_const_fields%ndtocn    
+    READ(75,NAME_OUTPUT)
+  write(nuout,*) 'Read Namelist OUTPUT'    
+  
+  ! Call routine to copy constants and logicals needed for ocean
+  ! physics into the kpp_const_fields derived type.  Added for 
+  ! compatability with OpenMP DEFAULT(private). NPK 8/2/13
+  CALL mckpp_initialize_constants(kpp_const_fields)    
+  
+  RETURN
+END SUBROUTINE MCKPP_INITIALIZE_NAMELIST
