e2cb26ae2c6559d779ab867f56d51af35301c8ff
aosprey <4313200+aosprey@users.noreply.github.com>
2021-02-25 15:59:14 +0000

    Define physics timers on first timestep.

--- mc-kpp_physics_driver.F90
+++ mc-kpp_physics_driver.F90
@@ -51,13 +51,15 @@ SUBROUTINE mckpp_physics_driver(kpp_3d_fields,kpp_const_fields)
 !$OMP SHARED(nz, nzp1, nx, ny, npts, nvel, nsclr, nvp1, nsp1, itermax) &
 !$OMP SHARED(hmixtolfrac, nztmax, nzp1tmax, nsflxs, njdt, maxmodeadv) &
 !$OMP PRIVATE(trans_timer_name, phys_timer_name, tid, index, kpp_1d_fields)
-!$OMP CRITICAL
   tid=OMP_GET_THREAD_NUM()
   WRITE(trans_timer_name,'(A17,I2)') 'KPP 3D/1D thread ',tid
-  CALL mckpp_define_new_timer(trans_timer_name, index)
   WRITE(phys_timer_name,'(A19,I2)') 'KPP Physics thread ',tid
-  CALL mckpp_define_new_timer(phys_timer_name, index)
+  IF (kpp_const_fields%ntime .EQ. 1) THEN
+!$OMP CRITICAL
+    CALL mckpp_define_new_timer(trans_timer_name, index)
+    CALL mckpp_define_new_timer(phys_timer_name, index)
 !$OMP END CRITICAL
+  END IF 
 !$OMP DO SCHEDULE(dynamic)
 #else
   WRITE(trans_timer_name,'(A19)') 'KPP 3D/1D thread 01'
