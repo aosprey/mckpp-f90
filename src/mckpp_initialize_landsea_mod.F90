996514207cd0350eef648ecb247b09d7cf8e44e7
aosprey <4313200+aosprey@users.noreply.github.com>
2021-05-11 12:21:51 +0100

    Move all setting of lats, lons, and land-sea into initalize_landsea routine.

--- src/mckpp_initialize_landsea_mod.F90
+++ src/mckpp_initialize_landsea_mod.F90
@@ -22,7 +22,7 @@ SUBROUTINE mckpp_initialize_landsea()
 #endif
   USE mckpp_netcdf_read, ONLY: mckpp_netcdf_open, mckpp_netcdf_close, &
       mckpp_netcdf_determine_boundaries, mckpp_netcdf_get_var
-  USE mckpp_log_messages, ONLY: mckpp_print, max_message_len
+  USE mckpp_log_messages, ONLY: mckpp_print, mckpp_print_warning, max_message_len
   USE mckpp_parameters, ONLY: npts, nx, ny
  
   IMPLICIT NONE
@@ -42,31 +42,33 @@ SUBROUTINE mckpp_initialize_landsea()
   REAL, DIMENSION(ny) :: lat_in
   CHARACTER(LEN=24) :: routine = "MCKPP_INITIALIZE_LANDSEA"
   CHARACTER(LEN=max_message_len) :: message
-  
-  IF (kpp_const_fields%L_LANDSEA) THEN
-     WRITE(message,*) "Reading", kpp_const_fields%landsea_file
-     CALL mckpp_print(routine, message)
-     
+
+#ifdef MCKPP_CAM3
+  DO ichnk=begchunk,endchunk
+     ncol=get_ncols_p(ichnk)
+     CALL GET_RLON_ALL_P(ichnk,ncol,clon1)
+     CALL GET_RLAT_ALL_P(ichnk,ncol,clat1)
+     kpp_3d_fields(ichnk)%dlon(:)=clon1*360./kpp_const_fields%twopi    
+     kpp_3d_fields(ichnk)%dlat(:)=clat1*360./kpp_const_fields%twopi
+  ENDDO
+#else
+  kpp_3d_fields%dlon(1)=kpp_const_fields%alon
+  kpp_3d_fields%dlat(1)=kpp_const_fields%alat
+#endif
+
+  IF (kpp_const_fields%L_LANDSEA) THEN     
 #ifdef MCKPP_CAM3
      IF (masterproc) THEN
-#endif      
+#endif
+       WRITE(message,*) "Reading", kpp_const_fields%landsea_file
+       CALL mckpp_print(routine, message)
        CALL mckpp_netcdf_open(routine, kpp_const_fields%landsea_file, ncid)
        CALL mckpp_netcdf_determine_boundaries(routine, kpp_const_fields%landsea_file, ncid, &
            kpp_3d_fields%dlon(1), kpp_3d_fields%dlat(1), offset_lon, offset_lat)
-
-#ifndef MCKPP_CAM3       
        CALL mckpp_netcdf_get_var(routine, kpp_const_fields%landsea_file, ncid, &
            "longitude", lon_in, offset_lon)
        CALL mckpp_netcdf_get_var(routine, kpp_const_fields%landsea_file, ncid, &
            "latitude", lat_in, offset_lat)
-       DO j = 1, ny 
-         DO i =1, nx
-           ipt = (i-1)*nx + j
-            kpp_3d_fields%dlon(ipt) = lon_in(i) 
-            kpp_3d_fields%dlat(ipt) = lat_in(j)
-         ENDDO
-       ENDDO
-#endif      
        start(1) = offset_lon
        start(2) = offset_lat
        count(1) = nx
@@ -76,9 +78,9 @@ SUBROUTINE mckpp_initialize_landsea()
        CALL mckpp_netcdf_get_var(routine, kpp_const_fields%landsea_file, ncid, &
            "maxdepth", ocdepth, start, count)       
        CALL mckpp_netcdf_close(routine, kpp_const_fields%landsea_file, ncid)
-     
+       
 #ifdef MCKPP_CAM3
-     ENDIF
+     ENDIF    
      CALL scatter_field_to_chunk(1,1,1,PLON,ocdepth,ocdepth_chunk(1,begchunk))
      CALL scatter_field_to_chunk(1,1,1,PLON,landsea,landsea_chunk(1,begchunk))
      DO ichnk=begchunk,endchunk
@@ -92,23 +94,46 @@ SUBROUTINE mckpp_initialize_landsea()
               kpp_3d_fields(ichnk)%L_OCEAN(icol)=.TRUE.
            ENDIF
         ENDDO
-     ENDDO
-#else
-     DO ipt=1,npts
-        IF (landsea(ipt) .EQ. 1.0) THEN
-           kpp_3d_fields%L_OCEAN(ipt)=.FALSE.
-        ELSE
-           kpp_3d_fields%L_OCEAN(ipt)=.TRUE.
-        ENDIF
       ENDDO
-      kpp_3d_fields%ocdepth = ocdepth
-#endif     
-  ELSE
+   ELSE
      DO ipt=1,npts
         kpp_3d_fields%L_OCEAN(ipt)=.FALSE.
      ENDDO
   ENDIF
-  
+     
+#else
+     DO j = 1, ny 
+       DO i =1, nx
+         ipt = (j-1)*nx + i
+         kpp_3d_fields%dlon(ipt) = lon_in(j) 
+         kpp_3d_fields%dlat(ipt) = lat_in(i)
+         IF (landsea(ipt) .EQ. 1.0) THEN
+           kpp_3d_fields%L_OCEAN(ipt)=.FALSE.
+         ELSE
+           kpp_3d_fields%L_OCEAN(ipt)=.TRUE.
+         ENDIF  
+       ENDDO
+     ENDDO
+     kpp_3d_fields%ocdepth = ocdepth
+     
+   ELSEIF (kpp_const_fields%L_REGGRID) THEN
+     DO j = 1, ny
+        DO i = 1, nx
+           ipt = (j-1)*nx + i
+           kpp_3d_fields%dlon(ipt) = kpp_const_fields%alon+(i-1)*kpp_const_fields%delta_lon
+           kpp_3d_fields%dlat(ipt) = kpp_const_fields%alat+(j-1)*kpp_const_fields%delta_lat
+           kpp_3d_fields%ocdepth(ipt) = -10000.
+           kpp_3d_fields%L_OCEAN(ipt) = .TRUE.
+        ENDDO
+      ENDDO
+      
+   ELSEIF (.NOT. kpp_const_fields%L_REGGRID .AND. .NOT. kpp_const_fields%L_LANDSEA) THEN
+     WRITE(message,*) "If you set L_REGGRID=.FALSE., you must specify a land-sea mask file from which", & 
+         " to read the locations of the gridpoints in the horizontal."
+     CALL mckpp_print_warning(routine, message)
+  ENDIF      
+#endif        
+ 
 END SUBROUTINE mckpp_initialize_landsea
 
 END MODULE mckpp_initialize_landsea_mod
