9682b7199a21bdb64fbfb801e5a32c3b5e0a2b02
Annette Osprey <4313200+aosprey@users.noreply.github.com>
2021-08-09 12:25:18 +0100

    Use routine to get time from timestep in XIOS restart write.

--- mc-kpp_xios_control.F90
+++ mc-kpp_xios_control.F90
@@ -3,6 +3,7 @@ MODULE mckpp_xios_control
 
   USE mckpp_data_fields, ONLY: kpp_3d_fields,kpp_const_fields
   USE mckpp_log_messages, ONLY: mckpp_print, max_message_len
+  USE mckpp_time_control, ONLY: mckpp_get_time
   USE mckpp_timer, ONLY: mckpp_start_timer, mckpp_stop_timer
   USE mckpp_xios_io, ONLY: xios_comm, ctx_hdl_diags, &
       mckpp_xios_diagnostic_definition, mckpp_xios_diagnostic_output, &
@@ -63,7 +64,7 @@ CONTAINS
 
       ! Set correct time for validity of restart file
       ! (end of this timestep = start of next timestep)
-      restart_time=kpp_const_fields%time+kpp_const_fields%dto/kpp_const_fields%spd
+      restart_time = mckpp_get_time(kpp_const_fields%ntime+1)
 
       WRITE(message,*) "Writing restart at time ", restart_time
       CALL mckpp_print(routine, message)
