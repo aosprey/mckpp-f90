bf9b67b221dab7084626d42511cb540842531a27
Annette Osprey <4313200+aosprey@users.noreply.github.com>
2021-08-10 09:23:42 +0100

    Reorganise read_sst module, and apply structure to read_ice.

--- src/mckpp_read_ice_mod.F90
+++ src/mckpp_read_ice_mod.F90
@@ -5,13 +5,6 @@
 
 MODULE mckpp_read_ice_mod
 
-  USE mckpp_abort_mod, ONLY: mckpp_abort
-  USE mckpp_netcdf_subs
-
-CONTAINS 
-
-SUBROUTINE mckpp_read_ice()
-
 #ifdef MCKPP_CAM3
   USE shr_kind_mod,only: r8=>shr_kind_r8
   USE mckpp_types,only: kpp_3d_fields,kpp_const_fields
@@ -21,190 +14,167 @@ SUBROUTINE mckpp_read_ice()
 #else
   USE mckpp_data_fields, ONLY: kpp_3d_fields, kpp_const_fields
 #endif  
+  USE mckpp_abort_mod, ONLY: mckpp_abort
   USE mckpp_log_messages, ONLY: mckpp_print, mckpp_print_error, max_message_len
+  USE mckpp_netcdf_read, ONLY: max_nc_filename_len, mckpp_netcdf_open, mckpp_netcdf_close, &
+      mckpp_netcdf_determine_boundaries, mckpp_netcdf_get_coord, mckpp_netcdf_get_var
   USE mckpp_parameters, ONLY: nx, ny, nx_globe, ny_globe
+  USE mckpp_time_control, ONLY: mckpp_get_update_time
 
-  ! Read in ice concentrations from a user-provided netCDF file.
-  ! Called _only_ if L_CLIMICE is TRUE in 3D_ocn.nml.
-  ! Probably only necessary when coupling the model to an atmosphere
-  ! that requires realistic ice concentrations.
-  ! Written by Nick Klingaman, 11/01/08.
-  
   IMPLICIT NONE
-#include <netcdf.inc>
+
+  PUBLIC mckpp_read_ice
+
+  PRIVATE
+
+  LOGICAL :: l_initialized = .FALSE. 
+  INTEGER :: ice_nx, ice_ny, num_times
+  INTEGER, DIMENSION(3) :: start, count
+  REAL, DIMENSION(:), ALLOCATABLE :: file_times
+
+CONTAINS
+
+
+  SUBROUTINE initialize_ice(file, ncid)
+
+    CHARACTER(LEN=max_nc_filename_len), INTENT(IN) :: file
+    INTEGER, INTENT(IN) :: ncid
+
+    CHARACTER(LEN=20) :: routine = "MCKPP_INITIALIZE_ICE"
+    CHARACTER(LEN=max_message_len) :: message
 
 #ifdef MCKPP_CAM3
-  REAL(r8) :: ice_temp(PLON,PLAT), ice_chunk(PCOLS,begchunk:endchunk)
-  INTEGER :: ichnk,ncol,ico
+    IF (masterproc) THEN
 #endif
-
-  integer :: ice_nx,ice_ny
-  REAL :: max_ice,min_ice
-  REAL*4 :: iceclim_time,first_timein,last_timein,time_in
-  REAL*4, ALLOCATABLE :: var_in(:,:,:), latitudes(:), longitudes(:)
-  INTEGER count(3),start(3)
-  INTEGER ix,iy,status,ncid,varid,time_varid,lon_varid,lat_varid,time_dimid,&
-       lon_dimid,lat_dimid,ntime_file,nlon_file,nlat_file
-  CHARACTER(LEN=30) tmp_name
-
-  CHARACTER(LEN=14) :: routine = "MCKPP_READ_ICE"
-  CHARACTER(LEN=max_message_len) :: message
-  
-#ifdef MCKPP_CAM3
-  IF (masterproc) THEN
-#endif
+      WRITE(message,*) "Initializing ", TRIM(file)
+      CALL mckpp_print(routine, message)
 
 #ifdef MCKPP_COUPLE
-  ice_nx=NX_GLOBE
-  ice_ny=NY_GLOBE
+      ice_nx=NX_GLOBE
+      ice_ny=NY_GLOBE
 #else
-  ice_nx=NX
-  ice_ny=NY
-#endif     
+      ice_nx=NX
+      ice_ny=NY
+#endif
 
-  ALLOCATE( var_in(ice_nx,ice_ny,1) ) 
-  ALLOCATE( latitudes(NY_GLOBE) ) 
-  ALLOCATE( longitudes(NX_GLOBE) )
-  
-  count=(/ice_nx,ice_ny,1/)
-  start=(/1,1,1/)
-
-  ! Open the netCDF file and find the correct latitude,
-  ! longitude and time.
-  status=NF_OPEN(kpp_const_fields%ice_file,0,ncid)
-  IF (status .NE. NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
-
+      ! Work out start and count for each time entry
+      count=(/ice_nx,ice_ny,1/)
+      start=(/1,1,1/)
 #ifndef MCKPP_COUPLE
-  CALL MCKPP_DETERMINE_NETCDF_BOUNDARIES(ncid,'ice climatology','latitude','longitude',&
-       't',kpp_3d_fields%dlon(1),kpp_3d_fields%dlat(1),start(1),start(2),first_timein,last_timein,time_varid)
-#else
-  IF (status .NE. NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
-  status=NF_INQ_VARID(ncid,'t',time_varid)
-  IF (status .NE. NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
-  status=NF_INQ_DIMID(ncid,'t',time_dimid)
-  IF (status .NE. NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
-  status=NF_INQ_DIM(ncid,time_dimid,tmp_name,ntime_file)
-  IF (status .NE. NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
-  status=NF_GET_VAR1_REAL(ncid,time_varid,start(3),first_timein)
-  IF (status .NE. NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
-  status=NF_GET_VAR1_REAL(ncid,time_varid,ntime_file,last_timein)
-#endif      
-
-  status=NF_INQ_VARID(ncid,'iceconc',varid)
-  iceclim_time=kpp_const_fields%time+0.5*kpp_const_fields%dto/kpp_const_fields%spd*kpp_const_fields%ndtupdice       
-  IF (iceclim_time .gt. last_timein) THEN
-     IF (kpp_const_fields%L_PERIODIC_CLIMICE) THEN 
-        DO WHILE (iceclim_time .gt. last_timein)
-           iceclim_time=iceclim_time-kpp_const_fields%climice_period
-        ENDDO
-     ELSE
-        WRITE(message,*) "Time for which to read ice exceeds the last time in the netCDF file", &
-            " and L_PERIODIC_CLIMICE has not been specified."
-        CALL mckpp_print_error(routine, message) 
-        WRITE(message,*) "Attempting to read ice will lead to an error, so aborting now ..."
-        CALL mckpp_print_error(routine, message) 
-        CALL MCKPP_ABORT()
-     ENDIF
-  ENDIF
-      
-  WRITE(message,*) 'Reading climatological ICECONC for time ',iceclim_time
-  CALL mckpp_print(routine, message) 
-  start(3)=NINT((iceclim_time-first_timein)*kpp_const_fields%spd/&
-       (kpp_const_fields%dto*kpp_const_fields%ndtupdice))+1      
-  status=NF_GET_VAR1_REAL(ncid,time_varid,start(3),time_in)
-  IF (status .NE. NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
-  IF (abs(time_in-iceclim_time) .GT. 0.01*kpp_const_fields%dtsec/kpp_const_fields%spd) THEN
-     WRITE(message,*) 'Cannot find time,',iceclim_time,'in ice concentration climatology file'
-     CALL mckpp_print_error(routine, message) 
-     WRITE(message,*) 'The closest I came was',time_in
-     CALL mckpp_print_error(routine, message) 
-     CALL MCKPP_ABORT()
-  ENDIF
-
-  status=NF_GET_VARA_REAL(ncid,varid,start,count,var_in)
-  IF (status .NE. NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
-
-#ifdef MCKPP_CAM3
-  ice_temp=var_in(:,:,1)
-  ENDIF ! End of masterproc section
-  CALL scatter_field_to_chunk(1,1,1,PLON,ice_temp,ice_chunk(1,begchunk))
-  DO ichnk=begchunk,endchunk
-     ncol=get_ncols_p(ichnk)
-     kpp_3d_fields(ichnk)%iceconc(1:ncol)=ice_chunk(1:ncol,ichnk)
-  ENDDO
-#else
-  max_ice = -1000.
-  min_ice = 1000.
-  DO ix=1,ice_nx
-     DO iy=1,ice_ny
-        kpp_3d_fields%iceconc(ix,iy) = var_in(ix,iy,1)
-        !IF (kpp_3d_fields%iceconc(ix,iy) .gt. max_ice) max_ice = kpp_3d_fields%iceconc(ix,iy)
-        !IF (kpp_3d_fields%iceconc(ix,iy) .lt. min_ice) min_ice = kpp_3d_fields%iceconc(ix,iy)
-     ENDDO
-  ENDDO
+      CALL mckpp_netcdf_determine_boundaries(routine, file, ncid, &
+          kpp_3d_fields%dlon(1), kpp_3d_fields%dlat(1), start(1), start(2))
 #endif
 
-  
-  IF (kpp_const_fields%L_CLIM_ICE_DEPTH) THEN 
+      ! Read in time field
+      CALL mckpp_netcdf_get_coord(routine, file, ncid, "time", num_times)
+      ALLOCATE(file_times(num_times)) 
+      CALL mckpp_netcdf_get_var(routine, file, ncid, "time", file_times)
+
 #ifdef MCKPP_CAM3
-     IF (masterproc) THEN
+    ENDIF ! End of masterproc section
 #endif
-     status=NF_INQ_VARID(ncid,'icedepth',varid)
-     IF (status .NE. NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
-     WRITE(message,*) 'Reading climatological ICEDEPTH for time ',iceclim_time             
-     CALL mckpp_print(routine, message) 
-     status=NF_GET_VARA_REAL(ncid,varid,start,count,var_in)
-     IF (status .NE. NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
+    l_initialized = .TRUE.
+
+  END SUBROUTINE initialize_ice
+
+
+  SUBROUTINE mckpp_read_ice()
+
+    ! Read in ice concentrations from a user-provided netCDF file.
+    ! Called _only_ if L_CLIMICE is TRUE in 3D_ocn.nml.
+    ! Probably only necessary when coupling the model to an atmosphere
+    ! that requires realistic ice concentrations.
+    ! Written by Nick Klingaman, 11/01/08.
+
+    IMPLICIT NONE
+
 #ifdef MCKPP_CAM3
-     ice_temp=var_in(:,:,1)
-     ENDIF ! End of masterproc section
-     CALL scatter_field_to_chunk(1,1,1,PLON,ice_temp,ice_chunk(1,begchunk))
-     DO ichnk=begchunk,endchunk
+    REAL(r8) :: ice_temp(PLON,PLAT), ice_chunk(PCOLS,begchunk:endchunk)
+    INTEGER :: ichnk,ncol,ico
+#endif
+    CHARACTER(LEN=max_nc_filename_len) :: file
+    REAL :: update_time, max_ice, min_ice
+    INTEGER :: ncid
+    REAL, ALLOCATABLE, DIMENSION(:,:,:) :: var_in
+
+    CHARACTER(LEN=14) :: routine = "MCKPP_READ_ICE"
+    CHARACTER(LEN=max_message_len) :: message
+
+#ifdef MCKPP_CAM3
+    IF (masterproc) THEN
+#endif
+      file = kpp_const_fields%sst_file
+      CALL mckpp_netcdf_open(routine, file, ncid)
+
+      ! On first call, get file dimensions
+      IF (.NOT. l_initialized) CALL initialize_ice(file, ncid)
+      ALLOCATE( var_in(ice_nx,ice_ny,1) )
+
+      ! Work out time to read and check against times in file
+      CALL mckpp_get_update_time(file,  kpp_const_fields%time, kpp_const_fields%ndtupdsst, &
+          file_times, num_times, kpp_const_fields%L_PERIODIC_CLIMSST, kpp_const_fields%climsst_period, &
+          update_time, start(3))
+      WRITE(message,*) 'Reading climatological ice concentration for time ', update_time
+      CALL mckpp_print(routine, message)
+
+      ! Read and process data
+      CALL mckpp_netcdf_get_var(routine, file, ncid, "iceconc", var_in, start) 
+
+#ifdef MCKPP_CAM3
+      ice_temp=var_in(:,:,1)
+    ENDIF ! End of masterproc section
+    CALL scatter_field_to_chunk(1,1,1,PLON,ice_temp,ice_chunk(1,begchunk))
+    DO ichnk=begchunk,endchunk
+      ncol=get_ncols_p(ichnk)
+      kpp_3d_fields(ichnk)%iceconc(1:ncol)=ice_chunk(1:ncol,ichnk)
+    ENDDO
+#else
+    kpp_3d_fields%iceconc(:,:) = var_in(:,:,1)
+#endif
+
+    IF (kpp_const_fields%L_CLIM_ICE_DEPTH) THEN 
+#ifdef MCKPP_CAM3
+      IF (masterproc) THEN
+#endif
+        WRITE(message,*) 'Reading climatological ice depth for time ', update_time
+        CALL mckpp_print(routine, message)
+        CALL mckpp_netcdf_get_var(routine, file, ncid, "icedepth", var_in, start) 
+
+#ifdef MCKPP_CAM3
+        ice_temp=var_in(:,:,1)
+      ENDIF ! End of masterproc section
+      CALL scatter_field_to_chunk(1,1,1,PLON,ice_temp,ice_chunk(1,begchunk))
+      DO ichnk=begchunk,endchunk
         ncol=get_ncols_p(ichnk)
         kpp_3d_fields(ichnk)%icedepth(1:ncol)=ice_chunk(1:ncol,ichnk)
-     ENDDO
+      ENDDO
 #else
-     DO ix=1,ice_nx
-        DO iy=1,ice_ny
-           kpp_3d_fields%icedepth(ix,iy)=var_in(ix,iy,1)
-        ENDDO
-     ENDDO
+      kpp_3d_fields%icedepth(:,:) = var_in(:,:,1)
 #endif
-  ENDIF
+    ENDIF
 
-  IF (kpp_const_fields%L_CLIM_SNOW_ON_ICE) THEN
+    IF (kpp_const_fields%L_CLIM_SNOW_ON_ICE) THEN
 #ifdef MCKPP_CAM3
-     IF (masterproc) THEN
+      IF (masterproc) THEN
 #endif
-     status=NF_INQ_VARID(ncid,'snowdepth',varid)
-     IF (status.NE.NF_NOERR) CALL MCKPP_HANDLE_ERR(status)
-     WRITE(message,*) 'Reading climatological SNOWDEPTH for time ',iceclim_time     
-     CALL mckpp_print(routine, message) 
-     status=NF_GET_VARA_REAL(ncid,varid,start,count,var_in)
-     IF (status.NE.NF_NOERR) CALL MCKPP_HANDLE_ERR(status)     
+        WRITE(message,*) 'Reading climatological snow depth for time ', update_time
+        CALL mckpp_print(routine, message)
+        CALL mckpp_netcdf_get_var(routine, file, ncid, "snowdepth", var_in, start)
+
+        CALL mckpp_netcdf_close(routine, file, ncid)
 #ifdef MCKPP_CAM3
-     ice_temp=var_in(:,:,1)
-     ENDIF
-     CALL scatter_field_to_chunk(1,1,1,PLON,ice_temp,ice_chunk(1,begchunk))
-     DO ichnk=begchunk,endchunk
+        ice_temp=var_in(:,:,1)
+      ENDIF
+      CALL scatter_field_to_chunk(1,1,1,PLON,ice_temp,ice_chunk(1,begchunk))
+      DO ichnk=begchunk,endchunk
         ncol=get_ncols_p(ichnk)
         kpp_3d_fields(ichnk)%snowdepth(1:ncol)=ice_chunk(1:ncol,ichnk)
-     ENDDO
+      ENDDO
 #else
-     DO ix=1,ice_nx
-        DO iy=1,ice_ny
-           kpp_3d_fields%snowdepth(ix,iy)=var_in(ix,iy,1)
-        ENDDO
-     ENDDO
+      kpp_3d_fields%snowdepth(:,:) = var_in(:,:,1)
 #endif
-  ENDIF
-
-#ifdef MCKPP_CAM3
-  IF (masterproc) status=NF_CLOSE(ncid)
-#else
-  status=NF_CLOSE(ncid)
-#endif
-  
-END SUBROUTINE mckpp_read_ice
+    ENDIF
+    
+  END SUBROUTINE mckpp_read_ice
 
 END MODULE mckpp_read_ice_mod
--- src/mckpp_read_sst_mod.F90
+++ src/mckpp_read_sst_mod.F90
@@ -17,152 +17,138 @@ MODULE mckpp_read_sst_mod
   USE mckpp_abort_mod, ONLY: mckpp_abort
   USE mckpp_log_messages, ONLY: mckpp_print, mckpp_print_error, max_message_len
   USE mckpp_netcdf_read, ONLY: max_nc_filename_len, mckpp_netcdf_open, mckpp_netcdf_close, &
-       mckpp_netcdf_determine_boundaries, mckpp_netcdf_get_coord, mckpp_netcdf_get_var
+      mckpp_netcdf_determine_boundaries, mckpp_netcdf_get_coord, mckpp_netcdf_get_var
   USE mckpp_parameters, ONLY: nx, ny, nx_globe, ny_globe
   USE mckpp_time_control, ONLY: mckpp_get_update_time
 
   IMPLICIT NONE
 
-  PUBLIC mckpp_initialize_sst, mckpp_read_sst
+  PUBLIC mckpp_read_sst
 
   PRIVATE
 
+  LOGICAL :: l_initialized = .FALSE. 
   INTEGER :: sst_nx, sst_ny, num_times
   INTEGER, DIMENSION(3) :: start, count
   REAL, DIMENSION(:), ALLOCATABLE :: file_times
-  REAL :: first_timein, last_timein
-  LOGICAL :: file_open = .FALSE.
- 
+
 CONTAINS
 
-SUBROUTINE mckpp_initialize_sst()
+  SUBROUTINE initialize_sst(file, ncid)
 
-  INTEGER :: ncid
-  CHARACTER(LEN=max_nc_filename_len) :: file
+    CHARACTER(LEN=max_nc_filename_len), INTENT(IN) :: file
+    INTEGER, INTENT(IN) :: ncid
 
-  CHARACTER(LEN=20) :: routine = "MCKPP_INITIALIZE_SST"
-  CHARACTER(LEN=max_message_len) :: message
-  
+    CHARACTER(LEN=20) :: routine = "MCKPP_INITIALIZE_SST"
+    CHARACTER(LEN=max_message_len) :: message
+
 #ifdef MCKPP_CAM3
-  IF (masterproc) THEN
+    IF (masterproc) THEN
 #endif
-
+      WRITE(message,*) "Initializing ", TRIM(file)
+      CALL mckpp_print(routine, message)
+      
 #ifdef MCKPP_COUPLE
-  sst_nx = nx_globe 
-  sst_ny = ny_globe 
+      sst_nx = nx_globe 
+      sst_ny = ny_globe 
 #else
-  sst_nx = nx
-  sst_ny = ny 
+      sst_nx = nx
+      sst_ny = ny 
 #endif
-
-  file = kpp_const_fields%sst_file
-  WRITE(message,*) "Initializing ", TRIM(file)
-  CALL mckpp_print(routine, message)
-  CALL mckpp_netcdf_open(routine, file, ncid)
-  file_open = .TRUE. 
-  
-  ! Work out start and count for each time entry
-  count=(/sst_nx,sst_ny,1/)
-  start=(/1,1,1/)
+      
+      ! Work out start and count for each time entry
+      count=(/sst_nx,sst_ny,1/)
+      start=(/1,1,1/)
 #ifndef MCKPP_COUPLE
-  CALL mckpp_netcdf_determine_boundaries(routine, file, ncid, &
-       kpp_3d_fields%dlon(1), kpp_3d_fields%dlat(1), start(1), start(2))
+      CALL mckpp_netcdf_determine_boundaries(routine, file, ncid, &
+          kpp_3d_fields%dlon(1), kpp_3d_fields%dlat(1), start(1), start(2))
 #endif
-  
-  ! Read in time field
-  CALL mckpp_netcdf_get_coord(routine, file, ncid, "time", num_times)
-  ALLOCATE(file_times(num_times)) 
-  CALL mckpp_netcdf_get_var(routine, file, ncid, "time", file_times)
-  first_timein = file_times(1)
-  last_timein = file_times(num_times)
+
+      ! Read in time field
+      CALL mckpp_netcdf_get_coord(routine, file, ncid, "time", num_times)
+      ALLOCATE(file_times(num_times)) 
+      CALL mckpp_netcdf_get_var(routine, file, ncid, "time", file_times)
 
 #ifdef MCKPP_CAM3
-  ENDIF ! End of masterproc section
+    ENDIF ! End of masterproc section
 #endif
-  
-  ! Now read first entry
-  ! This should close file at end
-  CALL mckpp_read_sst()
- 
-END SUBROUTINE mckpp_initialize_sst
+    l_initialized = .TRUE.
+
+  END SUBROUTINE initialize_sst
 
 
-SUBROUTINE mckpp_read_sst()
-
-  CHARACTER(LEN=max_nc_filename_len) :: file
-  REAL :: update_time
-  INTEGER :: ncid, ix, iy, ipt
-  REAL :: offset_sst
-  REAL, ALLOCATABLE, DIMENSION(:,:,:) :: var_in
+  SUBROUTINE mckpp_read_sst()
 
 #ifdef MCKPP_CAM3
-  REAL(r8) :: sst_temp(PLON,PLAT), sst_chunk(PCOLS,begchunk:endchunk)
-  INTEGER :: ichnk,ncol,icol
+    REAL(r8) :: sst_temp(PLON,PLAT), sst_chunk(PCOLS,begchunk:endchunk)
+    INTEGER :: ichnk,ncol,icol
 #endif
- 
-  CHARACTER(LEN=14) :: routine = "MCKPP_READ_SST"
-  CHARACTER(LEN=max_message_len) :: message
-  
+    CHARACTER(LEN=max_nc_filename_len) :: file
+    REAL :: update_time
+    INTEGER :: ncid, ix, iy, ipt
+    REAL :: offset_sst
+    REAL, ALLOCATABLE, DIMENSION(:,:,:) :: var_in
+
+    CHARACTER(LEN=14) :: routine = "MCKPP_READ_SST"
+    CHARACTER(LEN=max_message_len) :: message
+
 #ifdef MCKPP_CAM3
-  IF (masterproc) THEN
+    IF (masterproc) THEN
 #endif
-     
-  ALLOCATE( var_in(sst_nx,sst_ny,1) )
+      file = kpp_const_fields%sst_file
+      CALL mckpp_netcdf_open(routine, file, ncid)
 
-  file = kpp_const_fields%sst_file
-  IF (.NOT. file_open) THEN
-     CALL mckpp_netcdf_open(routine, file, ncid)
-     file_open = .TRUE. 
-  ENDIF 
+      ! On first call, get file dimensions
+      IF (.NOT. l_initialized) CALL initialize_sst(file, ncid)
+      ALLOCATE( var_in(sst_nx,sst_ny,1) )
 
-  ! Work out time to read and check against times in file
-  CALL mckpp_get_update_time(file,  kpp_const_fields%time, kpp_const_fields%ndtupdsst, &
-      file_times, num_times, kpp_const_fields%L_PERIODIC_CLIMSST, kpp_const_fields%climsst_period, &
-      update_time, start(3))
-  WRITE(message,*) 'Reading climatological SST for time ', update_time
-  CALL mckpp_print(routine, message)
-  WRITE(message,*) 'Reading climatological SST from position ',start(3)
-  CALL mckpp_print(routine, message)
+      ! Work out time to read and check against times in file
+      CALL mckpp_get_update_time(file, kpp_const_fields%time, kpp_const_fields%ndtupdsst, &
+          file_times, num_times, kpp_const_fields%L_PERIODIC_CLIMSST, kpp_const_fields%climsst_period, &
+          update_time, start(3))
+      WRITE(message,*) 'Reading climatological SST for time ', update_time
+      CALL mckpp_print(routine, message)
+      WRITE(message,*) 'Reading climatological SST from position ',start(3)
+      CALL mckpp_print(routine, message)
 
-  CALL mckpp_netcdf_get_var(routine, file, ncid, "sst", var_in, start) 
- 
-  CALL mckpp_netcdf_close(routine, file, ncid)
-  file_open = .FALSE.
-  
-  ! KPP expects temperatures in CELSIUS.  If climatological SSTs are
-  ! in Kelvin, subtract 273.15.
-  offset_sst = 0.
-  DO ix = 1, sst_nx
-     DO iy = 1, sst_ny
-        IF (var_in(ix,iy,1) .gt. 200 .and. var_in(ix,iy,1) .lt. 400) &
-             offset_sst = 273.15
-     END DO
-  END DO
-  
+      ! Read data 
+      CALL mckpp_netcdf_get_var(routine, file, ncid, "sst", var_in, start) 
+      CALL mckpp_netcdf_close(routine, file, ncid)
+
+      ! KPP expects temperatures in CELSIUS.  If climatological SSTs are
+      ! in Kelvin, subtract 273.15.
+      offset_sst = 0.
+      DO ix = 1, sst_nx
+        DO iy = 1, sst_ny
+          IF (var_in(ix,iy,1) .gt. 200 .and. var_in(ix,iy,1) .lt. 400) &
+              offset_sst = 273.15
+        END DO
+      END DO
+
 #ifdef MCKPP_CAM3
-  sst_temp=var_in(:,:,1)-offset_sst
-  ENDIF ! End of masterproc section
-  CALL scatter_field_to_chunk(1,1,1,PLON,sst_temp,sst_chunk(1,begchunk))
-  DO ichnk=begchunk,endchunk
-     ncol=get_ncols_p(ichnk)
-     kpp_3d_fields(ichnk)%sst(1:ncol)=sst_chunk(1:ncol,ichnk)     
-     IF (.NOT. kpp_const_fields%L_CLIMICE) kpp_3d_fields(ichnk)%iceconc(:)=0.0
-     IF (.NOT. kpp_const_fields%L_CLIMCURR) THEN
+      sst_temp=var_in(:,:,1)-offset_sst
+    ENDIF ! End of masterproc section
+    CALL scatter_field_to_chunk(1,1,1,PLON,sst_temp,sst_chunk(1,begchunk))
+    DO ichnk=begchunk,endchunk
+      ncol=get_ncols_p(ichnk)
+      kpp_3d_fields(ichnk)%sst(1:ncol)=sst_chunk(1:ncol,ichnk)     
+      IF (.NOT. kpp_const_fields%L_CLIMICE) kpp_3d_fields(ichnk)%iceconc(:)=0.0
+      IF (.NOT. kpp_const_fields%L_CLIMCURR) THEN
         kpp_3d_fields(ichnk)%usf(:)=0.0
         kpp_3d_fields(ichnk)%vsf(:)=0.0
-     ENDIF
-  ENDDO
+      ENDIF
+    ENDDO
 #else
-  kpp_3d_fields%sst = var_in(:,:,1) - offset_sst          
-  IF (.NOT. kpp_const_fields%L_CLIMICE) THEN
-     kpp_3d_fields%iceconc = 0.0
-  ENDIF
-  IF (.NOT. kpp_const_fields%L_CLIMCURR) THEN
-    kpp_3d_fields%usf = 0.0
-    kpp_3d_fields%vsf = 0.0
-  ENDIF
+    kpp_3d_fields%sst = var_in(:,:,1) - offset_sst          
+    IF (.NOT. kpp_const_fields%L_CLIMICE) THEN
+      kpp_3d_fields%iceconc = 0.0
+    ENDIF
+    IF (.NOT. kpp_const_fields%L_CLIMCURR) THEN
+      kpp_3d_fields%usf = 0.0
+      kpp_3d_fields%vsf = 0.0
+    ENDIF
 #endif
-  
-END SUBROUTINE mckpp_read_sst
+
+  END SUBROUTINE mckpp_read_sst
 
 END MODULE mckpp_read_sst_mod
