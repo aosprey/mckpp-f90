d65791ebf77d2e49624ff67b3c05cf69875dce86
aosprey <4313200+aosprey@users.noreply.github.com>
2021-09-08 14:07:30 +0100

    Rewrite abort routine to write an error message then call MPI_abort.

--- src/mckpp_abort_mod.F90
+++ src/mckpp_abort_mod.F90
@@ -2,18 +2,25 @@ MODULE mckpp_abort_mod
   
   IMPLICIT NONE
 
+  USE mckpp_log_messages, ONLY: mckpp_print_stderr
+  USE mpi 
+  IMPLICIT NONE
+  
 CONTAINS 
 
-SUBROUTINE MCKPP_ABORT()
-  
-#ifdef OASIS3
-  ! Support for stopping in OASIS3
-  ! NPK 2/11/09 - R3
-  CALL mpi1_oasis3_terminate()
-#else
-  STOP
-#endif
-  
-END SUBROUTINE MCKPP_ABORT
+  ! Shutdown model. 
+  ! The error code passed to MPI_abort doesn't mean anything.
+  SUBROUTINE mckpp_abort(routine, message)
 
+    CHARACTER(LEN=*), INTENT(IN) :: routine, message
+    INTEGER :: error_code = 100
+    INTEGER :: ierr
+    
+    CALL mckpp_print_stderr(routine, message) 
+    
+    error_code = 100 
+    CALL mpi_abort(MPI_comm_world, error_code, ierr)
+ 
+  END SUBROUTINE mckpp_abort
+    
 END MODULE mckpp_abort_mod
