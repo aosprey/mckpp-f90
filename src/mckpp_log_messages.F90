30bc4feee5ac917b7407d419b281126d667137d7
Annette Osprey <4313200+aosprey@users.noreply.github.com>
2021-09-09 09:40:12 +0100

    Expose stdout, stderr, and log file units.

--- src/mckpp_log_messages.F90
+++ src/mckpp_log_messages.F90
@@ -8,12 +8,14 @@ MODULE mckpp_log_messages
 
   PUBLIC :: mckpp_initialize_logs, mckpp_finalize_logs, &
        mckpp_print, mckpp_print_error, mckpp_print_warning, &
-       mckpp_print_stderr, update_context, max_message_len
+       mckpp_print_stderr, update_context, max_message_len, &
+       nupe, nuout, nuerr
   
   PRIVATE 
 
-  INTEGER :: nuout = 6, nuerr = 0, nupe = 99, my_rank 
-  INTEGER, PARAMETER :: max_message_len = 250
+  INTEGER :: my_rank 
+  INTEGER, PARAMETER :: max_message_len = 250, &
+      nuout = 6, nuerr = 0, nupe = 99
 
 CONTAINS
 
